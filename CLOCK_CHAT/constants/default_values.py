4bded59d213c89dc0dae63f52ff0775030874d40
partha <partha.sutradharmy@gmail.com>
2025-03-12 18:58:34 +0530

    Refactor enums in default_values.py for improved readability and organization

--- CLOCK_CHAT/constants/default_values.py
+++ CLOCK_CHAT/constants/default_values.py
@@ -4,23 +4,28 @@ class Role(Enum):
     ADMIN = 1
     END_USER =2
 
+
 class Gender(Enum):
     MALE = 1
     FEMALE = 2
     OTHER = 3
 
+
 class Chat_Type(Enum):
     Personal = 1
     Group = 2
+
 
 class Delete_Type(Enum):
     FOR_ME = 1
     FOR_ALL = 2
 
+
 class Status_Type(Enum):
     IMAGE = 1
     VIDEO = 2
     TEXT = 3
+
 
 class ResponseMessageType(Enum):
     SUCCESS='success'
@@ -28,9 +33,10 @@ class ResponseMessageType(Enum):
     WARNING='warning'
     INFO='info'
     NONE='null'
-=========
+
+
 class Status_Type(Enum):
     IMAGE = 1
     VIDEO = 2
     TEXT = 3
->>>>>>>>> Temporary merge branch 2
+
