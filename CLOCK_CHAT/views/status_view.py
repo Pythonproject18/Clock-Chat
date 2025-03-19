83034fbfe650402d5f71685299c2428632e23d83
Subham <modaksubham866@gmail.com>
2025-03-19 16:09:35 +0530

    fix issue

--- CLOCK_CHAT/views/status_view.py
+++ CLOCK_CHAT/views/status_view.py
@@ -2,17 +2,15 @@ from django.contrib.auth.decorators import login_required
 from django.views import View
 from django.http import JsonResponse
 from CLOCK_CHAT.services import status_service
+from django.shortcuts import render
 
-@login_required
-class StatusListView(View):
-    
+class StatusListView(View):  # Use LoginRequiredMixin
     def get(self, request, *args, **kwargs):
         user = request.user.id  # Get logged-in user
         print(user)
         # Fetch friends (assuming a Friend model with a many-to-many relation)
         friends = status_service.get_friends_by_user(user)
 
-        # Fetch statuses of friends
-       
+        # Fetch statuses of friends (logic needed)
         
-        return
+        return render(request, "status_list.html", {"friends": friends})  # Use appropriate template
