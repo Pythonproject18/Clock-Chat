5ea8b33ec4c19c88149ff787e66f37f8eeae346e
Subham <modaksubham866@gmail.com>
2025-04-14 18:48:06 +0530

    "fix some issues"

--- CLOCK_CHAT/views/home_view.py
+++ CLOCK_CHAT/views/home_view.py
@@ -3,5 +3,7 @@ from django.shortcuts import redirect,render
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'enduser/home/landing.html', )
+        if request.user.is_authenticated:
+            return redirect('/chat/')
+        return render(request, 'enduser/home/landing.html')
     
