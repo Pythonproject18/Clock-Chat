c8e6fe500fa5218c748a0ef26c6a107d156c53eb
Subham <modaksubham866@gmail.com>
2025-04-15 18:31:03 +0530

    "profile ui fix"

--- CLOCK_CHAT/views/home_view.py
+++ CLOCK_CHAT/views/home_view.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.shortcuts import redirect,render
 from CLOCK_CHAT.services import user_service
+from django.http.response import JsonResponse
 
 class HomeView(View):
     def get(self, request):
@@ -9,7 +10,8 @@ class HomeView(View):
         return render(request, 'enduser/home/landing.html')
     
 class UserProfileView(View):
-    def get(self,request):
+    def get(self, request):
         user_id = request.user.id
-        user_details=user_service.get_user_details(user_id)
-        return
+        user_details = user_service.get_user_details(user_id)
+        print(user_details)
+        return JsonResponse(user_details, safe=False)
